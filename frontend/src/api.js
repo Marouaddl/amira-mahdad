3edba5475b03240a47204c0b603791e4603a915e
maroua <marouadjili2@gmail.com>
2025-09-07 17:28:07 +0100

    updatedddddd

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 console.log('REACT_APP_API_URL:', process.env.REACT_APP_API_URL); // Débogage
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL || 'https://amira-mahdad-backend.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
-});
+});;
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
